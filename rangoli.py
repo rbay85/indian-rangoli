07fcace42936efc8f2db2472a43831a898602e65
rbayramu <roman.bayramukov@t-systems.tu>
2020-06-03 23:24:08 +0300

    print alphabet rangoli

--- rangoli.py
+++ rangoli.py
@@ -5,5 +5,27 @@ import string
 def print_rangoli(size):
     if size == 1:
         print ("a")
-		
-print_rangoli(1)
+    else:
+        inc = 1
+        i = 1
+        while i > 0:
+            centre = (size-1)*2
+            for j in range (0, centre):
+                delta = ((size)*2-i*2)-j
+                if (delta%2 == 0 and j > centre-2*i):
+                    print(string.ascii_lowercase[2*size-1-(j//2)-i], end="")
+                else:
+                    print("-", end="")
+            print(string.ascii_lowercase[size-i], end="")
+            for j in range (centre-1, -1, -1):
+                delta = ((size)*2-i*2)-j
+                if (delta%2 == 0 and j > centre-2*i):
+                    print(string.ascii_lowercase[2*size-1-(j//2)-i], end="")
+                else:
+                    print("-", end="")
+            print()
+            i += inc
+            if i == size:
+                inc = -1
+
+print_rangoli(26)
